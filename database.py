af7c1e4bcc6cb32f3f4016cbb205bec6ce6fe039
Rahma Musse <rmusse@umich.edu>
2025-12-10 18:17:17 -0500

    debugging

--- database.py
+++ database.py
@@ -130,9 +130,9 @@ def main():
     with open("omdb_movies.json", "r") as f:
         omdb_movies = json.load(f)
 
-    # Limit to 25 items per execution
-    tmdb_movies = tmdb_movies[:25]
-    omdb_movies = omdb_movies[:25]
+    # # Limit to 25 items per execution
+    # tmdb_movies = tmdb_movies[:25]
+    # omdb_movies = omdb_movies[:25]
 
     # Insert TMDB data
     for movie in tmdb_movies:
